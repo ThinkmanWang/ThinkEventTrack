f6c720b93cd083de54a104bc7422284aef95ed86
Thinkman Wang <wangxf1985@gmail.com>
2021-09-17 14:59:16 +0800

    update

--- scripts/FirstSQL.py
+++ scripts/FirstSQL.py
@@ -48,9 +48,32 @@ def test2():
         # lstData.append(dictRow)
     print(pTable)
 
+def query_pv_by_date():
+    conn = connect(host='172.16.0.2', port=9002, path='/druid/v2/sql/', scheme='http')
+    curs = conn.cursor()
+    curs.execute('''
+        select 
+            TIME_FORMAT(__time, 'YYYY-MM-dd', '+08:00') as pv_date
+            , count(1) as cnt
+        from 
+            thinkeventtrack 
+        WHERE 
+            __time >= TIME_PARSE('2021-09-01 00:00:00', 'YYYY-MM-dd HH:mm:ss', '+08:00')
+            and __time <= TIME_PARSE('2021-09-30 23:59:59', 'YYYY-MM-dd HH:mm:ss', '+08:00')
+        GROUP BY
+            TIME_FORMAT(__time, 'YYYY-MM-dd', '+08:00')
+    ''')
+
+    pTable = PrettyTable(["pv_date", "cnt"])
+    for row in curs:
+        pTable.add_row([row.pv_date, row.cnt])
+
+    print(pTable)
+
 def main():
     test1()
     test2()
+    query_pv_by_date()
 
 if __name__ == '__main__':
     main()
